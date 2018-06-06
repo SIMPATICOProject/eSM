d6b4a048302faa65270bc591e42365dd46728475
paubert <paubert@hi-iberia.es>
2018-06-06 17:14:56 +0200

    [FIX] Improve error catching and logging and prepare word cloud

--- public/js/stats.js
+++ public/js/stats.js
@@ -44,31 +44,21 @@ $(document).ready(function(){
   });
 
 
-  //TODO: Analyse text to get most common words and "weigh" them
-  // WordCloud(
-  //   document.getElementById('cloud_canvas'),
-  //   {
-  //     list: [
-  //       ['entiendo', 20],
-  //       ['difícil', 16],
-  //       ['palabra', 14],
-  //       ['no', 14],
-  //       ['balneario', 14],
-  //       ['simple', 14],
-  //       ['esto', 14],
-  //       ['texto', 12]
-  //     ]
-  //   }
-  // );
+
+  var fontSizes = [24, 20, 20, 20, 18, 18, 18, 16, 16, 16, 16, 16, 14, 14, 14, 14, 14];
+
+  var list = [];
+  for(var i=0; i<fontSizes.length; i++){
+    if (cloud[i]) {
+      list.push([cloud[i], fontSizes[i]]);
+    }
+  }
+
+
   WordCloud(
     document.getElementById('cloud_canvas'),
     {
-      list: [
-        ['word', 20],
-        ['cloud', 16],
-        ['not', 14],
-        ['available', 14]
-      ]
+      list: list
     }
   );
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var request= require('request');
 var async = require('async');
 var express = require('express');
+var wf = require('word-freq');
 var router = express.Router();
 
 var properties = require('../properties');
@@ -16,42 +17,88 @@ router.get('/stats/:eservice?', function(req, res, next) {
   if(eservice){
     async.parallel([
       function(cb) {
-        requestLogs(eservice, "total_requests", function(err, result){
+        requestLogs(eservice, "total_requests", function(err, result, call){
+
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "ended_requests", function(err, result){
+        requestLogs(eservice, "ended_requests", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "faces", function(err, result){
+        requestLogs(eservice, "faces", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "logins", function(err, result){
+        requestLogs(eservice, "logins", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "ctzp", function(err, result){
+        requestLogs(eservice, "ctzp", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "simpl", function(err, result){
+        requestLogs(eservice, "simpl", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       },
       function(cb) {
-        requestLogs(eservice, "cdv", function(err, result){
+        requestLogs(eservice, "cdv", function(err, result, call){
+          if (err) return cb(err);
+          if (!result || (result && !result.hasOwnProperty('count'))) {
+            return cb(errorLog(call, "<object> {count: <number>, results: <array>}", result));
+          }
           cb(err, result);
         });
       }
     ], function(err, results){
       //TODO: Process result
+
+      // console.log("**************************");
+      // results.forEach(function(r){
+      //   console.log(r.count);
+      // });
+      // // console.log(results);
+      // console.log("**************************");
+
+
+      if (err) {
+        console.log(err);
+        next(err);
+        return;
+      }
+
+
+
 
       var faces = {
         total: 0,
@@ -59,33 +106,57 @@ router.get('/stats/:eservice?', function(req, res, next) {
         normal: 0,
         happy: 0
       };
-      results[2].results.forEach(function(obj){
-        faces[obj.data["faces-session-feedback"]]++;
-        faces.total++;
+      if (results[2]) {
+        results[2].results.forEach(function(obj){
+          if (faces.hasOwnProperty(obj.data["faces-session-feedback"])) {
+            faces[obj.data["faces-session-feedback"]]++;
+          } else {
+            faces[obj.data["faces-session-feedback"]] = 1;
+          }
+          faces.total++;
+        });
+      }
+
+
+      var total_sessions = results[3]? results[3].count : 0;
+      var use_ctzp = results[4]? results[4].count : 0;
+      var use_simpl = results[5]? results[5].count : 0;
+      var use_cdv = results[6]? results[6].count : 0;
+
+
+
+      //WORD CLOUD
+      var cloud = wf.freq("word word word word cloud cloud cloud not not available", false, false);
+      //TODO: Results[7] should be the whole text to word-cloud
+      // var cloud = wf.freq(results[7], true, false);
+
+      var topWords = Object.keys(cloud).sort(function(a, b){
+        return cloud[b] - cloud[a];
       });
 
 
-      var total_sessions = results[3].count;
-      var use_ctzp = results[4].count;
-      var use_simpl = results[5].count;
-      var use_cdv = results[6].count;
-
 
       var locals = {
-        total_requests: results[0].count,
-        ended_requests: results[1].count,
+        total_requests: results[0]? results[0].count : 0,
+        ended_requests: results[0]? results[1].count : 0,
         faces: JSON.stringify(faces),
-        use_ctzp: Math.floor((use_ctzp/total_sessions)*100),
-        use_simpl: Math.floor((use_simpl/total_sessions)*100),
-        use_cdv: Math.floor((use_cdv/total_sessions)*100),
+        use_ctzp: Math.floor((use_ctzp/(total_sessions==0? 1 : total_sessions))*100),
+        use_simpl: Math.floor((use_simpl/(total_sessions==0? 1 : total_sessions))*100),
+        use_cdv: Math.floor((use_cdv/(total_sessions==0? 1 : total_sessions))*100),
         json: JSON.stringify(results[4]),
 
         //TEMP
-        mean_time: 40,
-        average_age: 35.8,
-        useful_ctzp: 30,
-        useful_simpl: 23,
-        relevant_simpl: 70
+        // mean_time: 40,
+        // average_age: 35.8,
+        // useful_ctzp: 30,
+        // useful_simpl: 23,
+        // relevant_simpl: 70
+        mean_time: "N/A",
+        average_age: "N/A",
+        useful_ctzp: "N/A",
+        useful_simpl: "N/A",
+        relevant_simpl: "N/A",
+        word_cloud: JSON.stringify(topWords)
       };
 
       res.render('stats', locals, function(err, html){
@@ -108,14 +179,27 @@ router.get('/qandas/:eservice?', function(req, res, next) {
   if(eservice){
     async.parallel([
       function(cb) {
-        requestCTZP(eservice, null, 'questions', function(err, result){
-          console.log(result);
-          cb(err, result);
+        requestCTZP(eservice, null, 'questions', function(err, result, call){
+
+          if (err) return cb(err);
+          if (isNaN(result)) {
+            return cb(errorLog(call, "<number>", result));
+          }
+
+          cb(err, Number(result));
         });
       },
       function(cb) {
-        requestCTZP(eservice, null, 'answers', function(err, result){
-          console.log(result);
+        requestCTZP(eservice, null, 'answers', function(err, result, call){
+          // console.log("Answers request");
+          // console.log(result);
+          // console.log("-----------------");
+
+
+          if (!Array.isArray(result)) {
+            return cb(errorLog(call, "<array> [{answers: <array>, stars: <array>, tags: <array>}, ...]", result));
+          }
+
           cb(err, result);
         });
       }
@@ -125,45 +209,56 @@ router.get('/qandas/:eservice?', function(req, res, next) {
       var totalV = 0;
       var paragraphs = [];
 
-      console.log(results);
+      // console.log("ALL RESULTS");
+      // console.log(results);
+      // console.log("-----------------");
 
-      if (results.length > 1) {
-        results[1].forEach(function(obj){
-          totalA += obj.answers.length;
-          totalV += obj.stars.length;
+      //TODO: Check result data for correct format
 
-          //TODO: Should be regarding a particular paragraph :)
-          // For now though, let's assume questions == paragraphs
+      if (err) {
+        console.error(err);
+        next(err);
+      } else {
 
-          var paragraph = {
-            index: paragraphs.length+1,
-            text: "This is a temporary paragraph text. In future versions, the corresponding text will be shown here.",
-            questions: 1,
-            answers: obj.answers.length,
-            votes: obj.stars.length,
-            tags: obj.tags
-          };
-          paragraphs.push(paragraph);
+        if (results.length > 1 && Array.isArray(results[1])) {
+          results[1].forEach(function(obj){
+            totalA += obj.answers.length;
+            totalV += obj.stars.length;
 
+            //TODO: Should be regarding a particular paragraph :)
+            // For now though, let's assume questions == paragraphs
+
+            var paragraph = {
+              index: paragraphs.length+1,
+              text: "This is a temporary paragraph text. In future versions, the corresponding text will be shown here.",
+              questions: 1,
+              answers: obj.answers.length,
+              votes: obj.stars.length,
+              tags: obj.tags
+            };
+            paragraphs.push(paragraph);
+
+          });
+        }
+
+
+        var locals = {
+          total_questions: results.length > 1? results[0] : 0,
+          total_answers: totalA,
+          total_votes: totalV,
+          paragraphs: paragraphs,
+          json: results.length > 1? JSON.stringify(results[1]) : []
+        };
+
+        // console.log(locals);
+
+        res.render('qandas', locals, function(err, html){
+          if(err) next(err);
+          else res.send(html);
         });
       }
 
 
-
-      var locals = {
-        total_questions: results.length > 1? results[0] : 0,
-        total_answers: totalA,
-        total_votes: totalV,
-        paragraphs: paragraphs,
-        json: results.length > 1? JSON.stringify(results[1]) : []
-      };
-
-      console.log(locals);
-
-      res.render('qandas', locals, function(err, html){
-        if(err) next(err);
-        else res.send(html);
-      });
     });
   }else{
     res.render('qandas', function(err, html){
@@ -182,17 +277,17 @@ router.get('/:page/:eservice?', function(req, res, next) {
 
 
 router.post('/login', function(req, res, next) {
-  console.log("Logging in!");
+
+  console.log("Log in attempt by: " + req.body.username);
   req.session.hasSession = true;
 
   var loginCreds = properties.getLoginUser();
 
   if (loginCreds.username.localeCompare(req.body.username) == 0 && loginCreds.password.localeCompare(req.body.password) == 0) {
-
-    console.log("Success!!");
+    console.log("Log in successful!");
     res.send({success: true});
-    // res.redirect(properties.getBase_URL()+'stats');
   } else {
+    console.log("Log in failed!")
     res.send({error: true});
   }
 });
@@ -211,27 +306,27 @@ function requestCTZP (eservice, paragraph, type, callback){
   if(type == "questions") apicall = "/stats/questions/"+eservice+(paragraph? "/"+paragraph : "");
   if(type == "answers" || type == "votes") apicall = "/qae/questions/"+eservice+(paragraph? "/"+paragraph : "");
 
-  console.log(properties.getCTZP_URL()+apicall);
+  console.log("Request to: " + properties.getCTZP_URL()+apicall);
 
   request(properties.getCTZP_URL()+apicall, function(error, response, body) {
-    console.log(error);
-    if(error) return callback(error);
 
-    console.log("+++++++++++++++++++++++");
-    console.log(response);
-    console.log(body);
-    console.log("-----------------------");
-
-    if(typeof body === "string"){
-      try {
-        var objBody = JSON.parse(body);
-        return callback(null, objBody);
-      }catch(e){
-        return callback(null, body);
+    if(error) {
+      console.error("Error on: " + properties.getCTZP_URL()+apicall);
+      console.error(error);
+      return callback(error);
+    } else {
+      if(typeof body === "string"){
+        try {
+          var objBody = JSON.parse(body);
+          return callback(null, objBody, properties.getCTZP_URL()+apicall);
+        }catch(e){
+          return callback(null, body, properties.getCTZP_URL()+apicall);
+        }
+      }else{
+        return callback(null, body, properties.getCTZP_URL()+apicall);
       }
-    }else{
-      return callback(null, body);
     }
+
   });
 };
 
@@ -246,25 +341,38 @@ function requestLogs (eservice, type, callback){
   if(type == "simpl") apicall = "/logs/find?common="+eservice+"&search=simplification_start";
   if(type == "cdv") apicall = "/logs/find?common="+eservice+"&search=usedata";
 
-  console.log(properties.getLogs_URL()+apicall);
+  console.log("Request to: " + properties.getLogs_URL()+apicall);
 
   request(properties.getLogs_URL()+apicall, function(error, response, body) {
-    if(error) return callback(error);
-
-    console.log("Success!! "+properties.getLogs_URL()+apicall);
+    if (error) {
+      console.log("Error on: " + properties.getLogs_URL()+apicall);
+      console.log(error);
+      return callback(error);
+    }
 
     var objBody = null;
     if(typeof body === "string"){
       try {
         objBody = JSON.parse(body);
       }catch(e){
-        //Do nothing
         console.log(e);
       }finally{
-        return callback(null, objBody || body);
+        return callback(null, objBody || body, properties.getLogs_URL()+apicall);
       }
     }else{
-      return callback(null, body);
+      return callback(null, body, properties.getLogs_URL()+apicall);
     }
   });
+};
+
+
+
+
+function errorLog (call, expect, result) {
+
+  if (typeof expect === "object") expect = JSON.stringify(expect);
+
+  return Error( "Unexpected response to API call: " + call + ".\n"+
+                "Expected response: " + expect + "\n"+
+                "Actual response: " + JSON.stringify(result));
 };
